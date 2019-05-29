df336986d2d27f76cdbf932c8d2817377d4ebd39
Nick Volgas <nvolgas@volnix.local>
2019-05-29 13:45:39 -0500

    Removing unused images.

---
+++ source/javascripts/site.js
@@ -0,0 +1,257 @@
+$(function () {
+
+    // ---------------------------------------------- //
+    // Navbar
+    // ---------------------------------------------- //
+
+    $(document).scroll(function () {
+        if ($(window).scrollTop() >= $('header').offset().top) {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    });
+
+
+    // ---------------------------------------------- //
+    // Scroll Spy
+    // ---------------------------------------------- //
+
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 80
+    });
+
+    // ---------------------------------------------- //
+    // Preventing URL update on navigation link click
+    // ---------------------------------------------- //
+
+    $('.navbar-nav a, #scroll-down').bind('click', function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 1000);
+        e.preventDefault();
+    });
+
+    // ------------------------------------------------------ //
+    // styled Google Map
+    // ------------------------------------------------------ //
+
+    map();
+
+
+    // ------------------------------------------------------ //
+    // For demo purposes, can be deleted
+    // ------------------------------------------------------ //
+
+    var stylesheet = $('link#theme-stylesheet');
+    $("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
+    var alternateColour = $('link#new-stylesheet');
+
+    if ($.cookie("theme_csspath")) {
+        alternateColour.attr("href", $.cookie("theme_csspath"));
+    }
+
+    $("#colour").change(function () {
+
+        if ($(this).val() !== '') {
+
+            var theme_csspath = 'css/style.' + $(this).val() + '.css';
+
+            alternateColour.attr("href", theme_csspath);
+
+            $.cookie("theme_csspath", theme_csspath, {
+                expires: 365,
+                path: document.URL.substr(0, document.URL.lastIndexOf('/'))
+            });
+
+        }
+
+        return false;
+    });
+
+});
+
+
+// ------------------------------------------------------ //
+// styled Google Map
+// ------------------------------------------------------ //
+
+function map() {
+
+    var mapId = 'map',
+        mapCenter = [34.74, -92.28];
+
+    if ($('#' + mapId).length > 0) {
+
+        var map = L.map(mapId, {
+            center: mapCenter,
+            zoom: 12,
+            dragging: false,
+            scrollwheel: false,
+            navigationControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            tap: false,
+            scrollWheelZoom: false,
+            gestureHandling: 'none',
+            zoomControl: false
+        });
+
+        var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            subdomains: 'abcd',
+            minZoom: 0,
+            maxZoom: 20,
+            ext: 'png'
+        });
+
+        Stamen_TonerLite.addTo(map);
+
+        map.once('focus', function () {
+            map.scrollWheelZoom.enable();
+        });
+
+    }
+
+}
+
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('intro',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 150,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        }
+      },
+      "opacity": {
+        "value": 0.7,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": .1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 4,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+
+);
+
+$(document).ready(function() {
+  //Save references to elements. Don't do DOM walks in event handlers when not needed.
+  var $sendEmailEl = $('#send-email');
+  var $from = $('#from-email');
+  var $subjectEl = $('#subject');
+  var $bodyEl = $('#message');
+  function updateEmailLink() {
+      $sendEmailEl.attr('href', 'mailto:n.volgas@gmail.com?' +
+          'subject=' + encodeURIComponent($subjectEl.val()) +
+          '&body=' + encodeURIComponent($bodyEl.val()) +
+          '&cc=' + encodeURIComponent($from.val())
+      );
+      //console.log($sendEmailEl.attr('href'));
+  }
+  $('#subject,#body,#from-email').on('input', updateEmailLink);
+  updateEmailLink();
+});
