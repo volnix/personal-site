ca1dd13d81913a5de72783acde27a676523e89c8
Nick Volgas <nvolgas@amazon.com>
2023-10-06 09:48:45 -0500

    Initial.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from "react";
+import './App.scss';
+import runIcon from '@iconify-icons/bx/run';
+import vinylIcon from '@iconify-icons/tabler/vinyl';
+import { Icon } from "@iconify/react";
 
-function App() {
+const Header = lazy(() => import('./components/Header'));
+const About = lazy(() => import('./components/About'));
+const Skills = lazy(() => import('./components/Skills'));
+const Resume = lazy(() => import('./components/Resume'));
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <a href="https://running.nickvolgas.com" style={{ position: 'absolute', top: 10, right: 250, border: 0 }}>
+        <Icon icon={runIcon} style={{ fontSize: "300%", margin: "9% 5% 0 5%", color: "rgba(0, 0, 0, .5)" }}/>
+      </a>
+      <a href="https://records.nickvolgas.com"  style={{ position: 'absolute', top: 10, right: 195, border: 0 }}>
+        <Icon icon={vinylIcon} style={{ fontSize: "300%", margin: "9% 5% 0 5%", color: "rgba(0, 0, 0, .5)" }}/>
+      </a>
+      <Suspense fallback={<>Loading...</>}><Header/></Suspense>
+      <Suspense fallback={<>Loading...</>}><About/></Suspense>
+      <Suspense fallback={<>Loading...</>}><Skills/></Suspense>
+      <Suspense fallback={<>Loading...</>}><Resume/></Suspense>
+    </>
   );
 }
-
-export default App;
